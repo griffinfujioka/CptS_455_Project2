88022e67f23e1bd2db6bca9dd8ea93e6463e69ce
griffinfujioka <fujiokag@hotmail.com>
2013-11-04 20:37:50 -0800

    cleaned up, select() still fails

--- router.c
+++ router.c
@@ -10,7 +10,7 @@ static const int MAXPENDING = 5;                 // Maximum outstanding connecti
 
 static const int DEBUG = 1; 
 
-static int MAX_DESCRIPTORS = 0; 
+static int MAX_DESCRIPTOR = 0; 				// Highest file descriptor 
 
 static int connectedNeighborSocket[MAXPAIRS]; 
 
@@ -65,26 +65,39 @@ routerInfo* GetRouterInfo(char* router)
 
 int main(int argc, char* argv[])
 {
-	char* directory; 					// directory to look for input files 
-	char* router;						// name of this router instance 
+	/* Directory to look for input files */ 
+	char* directory; 	
+
+	/* Name of this router instance */ 				
+	char* router;		
+
+	/* temp set of file descriptors for select() */ 	
+	fd_set rfds;		
+
+	/* master file descriptor set */ 
+	/* will be shadow copied into rfds on each iteration */ 
+	fd_set masterFD_Set; 
+
+	/* array of all file descriptors */ 
+	 int servSock[MAXROUTERS]; 
+
+	struct timeval tv;					// timeout boundary 
+	int retval;							// return value of select()
+    int readyDescriptors = 0; 			// number of descriptors with available data 
+	char messageBuffer[1024]; 
+	ssize_t numBytes = 0; 
+
 	routerInfo* routerConfiguration; 	// name, host, baseport 
 	linkInfo* routerLinks; 				// name, cost, local links, remote links		
 	neighborSocket* neighbors; 			// name, socket 
-	ssize_t numBytes = 0; 
+	
 	in_port_t receivingPort; 			// port for receiving messages 
 	int receivingSocket;                 // Socket descriptor 
-	fd_set rfds;						// temp set of file descriptors for select()
-	/* master file descriptor set - will be shadow copied into rfds on each iteration */ 
-	fd_set masterFD_Set; 	
-    struct timeval tv;					// timeout boundary 
-    int retval;							// return value of select()
-    int readyDescriptors = 0; 			// number of descriptors with available data 
+    
     struct sockaddr_in neighborAddr;    // buffer for addresses neighboring router
     int neighborSock; 					// socket for neighboring router 
     socklen_t neighborAddrLength = sizeof(neighborAddr);	// Set length of client address structure (in-out parameter)
-    int servSock[MAXROUTERS]; 			// Data structure containing all of the file descriptors 
-
-	char messageBuffer[1024]; 
+   
 
 	int i = 0; 
 
@@ -113,22 +126,28 @@ int main(int argc, char* argv[])
 	}
 	
 
-	// Read all routers in the system 
+	/************************************/ 
+	/* 	Read all routers in the network */ 
+	/************************************/ 
 	routerConfiguration = readrouters(directory); 
 
-	// Read the links for this router 
+	/************************************/ 
+	/* Read the links for this router 	*/  
+	/************************************/ 
 	routerLinks = readlinks(directory, router); 
 
-	// Should I initialize my routing table here or am I set?  
+	// Should I initialize my routing table here or am I good to go?  
 
-	// Create connected datagram sockets for talking to neighbors
-	// and provide us an array of (neighbor, socket) pairs
-	// createConnections takes care of 
-	// 		(1) socket
-	// 		(2) bind
-	// 		(3) connect 
-	// Which means we need to take care of: 
-	// 		(1) send/recv
+	/****************************************************************/ 
+	/* Create connected datagram sockets for talking to neighbors  	*/ 
+	/* and provide us an array of (neighbor, socket) pairs			*/ 
+	/* createConnections takes care of 								*/ 
+	/* 		(1) socket 												*/ 
+	/* 		(2) bind 												*/ 
+	/* 		(3) connect 											*/ 
+	/* Which means we (as a client) need to take care of: 						*/ 
+	/* 		(1) send/recv 											*/ 
+	/****************************************************************/ 
 	neighbors = createConnections(router); 
 
 	/************************************/ 
@@ -138,100 +157,125 @@ int main(int argc, char* argv[])
 	FD_ZERO(&rfds);					// clear the temp file descriptor set  
 	FD_SET(0, &masterFD_Set); 		// add file descriptor 0 for keyboard to masterFD_Set
 
+
 	if(DEBUG)
 	{
-
-
 		printf("\nSuccessfully constructed datagram sockets for each of my %d neighbors...", count); 
-
 		printf("\nRouters in the system: ");
+	}
 
-		while(i < MAXROUTERS)
+	while(i < MAXROUTERS)
+	{
+		
+		routerConfiguration = &routerInfoTable[i];
+		if(routerConfiguration->baseport == 0)
+			break; 
+
+		if(DEBUG)
 		{
-			
-			routerConfiguration = &routerInfoTable[i];
-			if(routerConfiguration->baseport == 0)
-				break; 
-
 			printf("\n%d. ", i + 1); 
 			printf("\nRouter: %s", routerConfiguration->router); 
 			printf("\nHost: %s", routerConfiguration->host); 
 			printf("\nBase port: %d", routerConfiguration->baseport); 
-			printf("\n"); 
+			printf("\n");
+		}
+		 
 
-			// This is kind of a hacky way to find my baseport but it works... 
-			if(strncmp(routerConfiguration->router, router, 1) == 0)
+		// This is kind of a hacky way to find my baseport but it works... 
+		if(strncmp(routerConfiguration->router, router, 1) == 0)
+		{
+			if(DEBUG)
 			{
-				if(DEBUG)
-				{
-					// Set our baseport so we can bind a socket to it later 
-					printf("\nSet my baseport to %d\n", routerConfiguration->baseport); 
-				}
-				
+				// Set our baseport so we can bind a socket to it later 
+				printf("\nMy name is %s and my host is %s", routerConfiguration->router, routerConfiguration->host); 
+				printf("\nSet my baseport to %d\n", routerConfiguration->baseport); 
 			}
 			
-			i++; 
 		}
+		
+		i++; 
+	}
 
+	if(DEBUG)
 		printf("\nConnections: "); 
-		i = 0; 
-		while(i < MAXLINKS)
+	i = 0; 
+	while(i < MAXLINKS)
+	{
+		routerLinks = &linkInfoTable[i]; 
+
+		if(routerLinks->cost == 0)
+			break; 
+
+		if(DEBUG)
 		{
-			routerLinks = &linkInfoTable[i]; 
-
-			if(routerLinks->cost == 0)
-				break; 
-
 			printf("\n%d.", i + 1); 
 			printf("\nRouter: %s", routerLinks->router); 
 			printf("\nCost: %d", routerLinks->cost); 
 			printf("\nLocal link: %d", routerLinks->locallink); 
 			printf("\nRemote link: %d", routerLinks->remotelink); 
 			printf("\n"); 
-			i++; 
-
 		}
+		
+		i++; 
+
+	}
+
+	if(DEBUG)
+	{
+		printf("\nNeighbor sockets: "); 
+	}
+	
+	i = 0; 
+	while(i < MAXPAIRS)
+	{
+		neighbors = &neighborSocketArray[i]; 
+
+
 
 		
+
+		if(neighbors->neighbor == 0)
+		{
+			servSock[i] == 0; 
+			break; 
+		}
+
 		/**********************************************/ 
 		/* Fill the servSock[] array with the sockets */ 
 		/**********************************************/ 
+		servSock[neighbors->socket] = neighbors->socket; 
+		FD_SET(neighbors->socket, &masterFD_Set); 	// add the socket file descriptor to the master FD set
+		MAX_DESCRIPTOR = neighbors->socket; 
+		
 
-		printf("\nNeighbor sockets: "); 
-		i = 0; 
-		while(i < MAXPAIRS)
+		if(DEBUG)
 		{
-			neighbors = &neighborSocketArray[i]; 
-
-
-
-			
-
-			if(neighbors->neighbor == 0)
-				break; 
-
 			printf("\n%d.", i + 1); 
 			printf("\nNeighbor: %s", neighbors->neighbor); 
 			printf("\nSocket: %d", neighbors->socket); 
-			// add the socket file descriptor to the master FD set
-			FD_SET(neighbors->socket, &masterFD_Set); 	
 			printf("\nAdded socket %d to master file descriptor set", neighbors->socket); 
-			MAX_DESCRIPTORS++; 
-			printf("\nIncremented MAX_DESCRIPTORS to %d", MAX_DESCRIPTORS); 
-
-			
-
-			
-
+			printf("\nUpdated MAX_DESCRIPTOR to %d", MAX_DESCRIPTOR); 
 			printf("\n"); 
+		}
 
 
-			i++; 
+		i++; 
+	}
+
+	/***********************************************/ 
+	/* Print out all the sockets in servSock array */ 
+	/***********************************************/ 
+	if(DEBUG)
+	{
+		for(i=0; i < MAX_DESCRIPTOR + 1; i++)
+		{
+			printf("\nservSock[%d] : %d", i, servSock[i]); 
 		}
 	}
 
-	
 
+	
+	// Do we need to setup the receiving socket? 
 	// Setup the receiving socket. Bind it to the local baseport
 	// to receive L and P messages (but not U messages) 
 	// That means we have to: 
@@ -240,9 +284,6 @@ int main(int argc, char* argv[])
 	// 		(3) listen() for connections 
 	// 		(4) accept() a connection 
    	// Create socket for incoming connections 
-
-    // Keep a master version of the set of file descriptors,
-    // Shadow copy it into &rfds each time 
 
 
 
@@ -257,20 +298,32 @@ int main(int argc, char* argv[])
 		printf("\n--\n--"); 
 		printf("\nWaiting for updates from fellow routers in my network..."); 
 
-        FD_ZERO(&rfds);			// clear the temp file descriptor set  
-        /*************************************************/ 
+		/*************************************************/ 
         /* Copy the masterFD_Set over to the temp FD set */ 
-        /*************************************************/ 
-        memcpy(&rfds, &masterFD_Set, sizeof(masterFD_Set)); 
+        /*************************************************/
+        FD_ZERO(&rfds);			// clear the temp file descriptor set  
+
+        for(i=0; i < MAX_DESCRIPTOR + 1; i++)
+        {
+        	if(servSock[i] != 0)
+        	{
+        		FD_SET(servSock[i], &rfds); 
+
+        		if(DEBUG)
+        			printf("\nAdded FD #%d to temporary file descriptor set", servSock[i]); 
+        	}
+        	
+        }
 
 
    		/* Wait up to 30 seconds. */
-   		tv.tv_sec = 5;
+   		tv.tv_sec = 30;
        	tv.tv_usec = 0;
 
        	if(DEBUG)
        		printf("\nWaiting on select()..."); 
-	   	retval = select(MAX_DESCRIPTORS+1, &rfds, NULL, NULL, &tv);
+
+	   	retval = select(MAX_DESCRIPTOR + 1, &rfds, NULL, NULL, &tv);
 
        	if(retval < 0)
        	{
@@ -289,16 +342,16 @@ int main(int argc, char* argv[])
        	/* One or more of the descriptors have data for us 	*/ 
        	/* We must determine which ones they are. 			*/ 
        	/****************************************************/ 
-
        	readyDescriptors = retval; 
-       	for(i=0; i <= MAX_DESCRIPTORS && readyDescriptors > 0; ++i)
+
+       	for(i=0; i <= MAX_DESCRIPTOR && readyDescriptors > 0; ++i)
        	{
        		if(DEBUG)
        			printf("\nChecking to see if FD %d has data for us", i); 
        		/****************************************************/ 
        		/* Check to see if this descriptor is ready 			*/ 
        		/****************************************************/ 
-       		if(FD_ISSET(i, &rfds))
+       		if(FD_ISSET(servSock[i], &rfds))
        		{
        			if(DEBUG)
        			{
